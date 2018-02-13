04dd07d84dd4ec8d9878c86c621bb37b72eca1d7
Rafael Specht <rafaelspdasilva@gmail.com>
2018-02-13 04:29:28 -0200

    Fixing tests

--- test/analysis-utils-test.js
+++ test/analysis-utils-test.js
@@ -2,11 +2,31 @@ const { describe, it, expect } = require('./setup')
 const analysisUtils = require('../analysis-utils')
 
 describe('analysis utils', () => {
-  it('who', () => {
-    expect(1).to.be.equal(1)
-    analysisUtils.getLevels('paul', 'test/fixtures/')
-      .then((levels) => {
-        console.log(levels[1])
-      })
+  describe('getLevels', () => {
+    it('get levels data', (done) => {
+      analysisUtils.getLevels('paul', 'test/fixtures/')
+        .then((levels) => {
+          expect(levels).to.have.length(2)
+          expect(levels[0]).to.have.length(4)
+          expect(levels[1]).to.have.length(4)
+
+          expect(levels[0]).to.eql([
+            { id: 1, screen_name: 'ringo' },
+            { id: 2, screen_name: 'george' },
+            { id: 3, screen_name: 'john' },
+            { id: 5, screen_name: 'michaelJackson' }
+          ])
+
+          const followedByRingo = levels[1][0].map((user) => user.screen_name)
+          const followedByGeorge = levels[1][1].map((user) => user.screen_name)
+          const followedByMichael = levels[1][3].map((user) => user.screen_name)
+
+          expect(followedByRingo).to.eql(['paul', 'george', 'john'])
+          expect(followedByGeorge).to.eql(['paul', 'john', 'ringo'])
+          expect(followedByMichael).to.eql(['paul', 'janetJackson'])
+
+          done()
+        })
+    })
   })
 })
